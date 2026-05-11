477015b274219a56cb612cd296b1604d30ed92cc
Yann <yannlabb@gmail.com>
2026-05-11 00:49:49 -0700

    Automatically find a project's .nvim.lua, remove the duplicate group local variable

--- nvim/init.lua
+++ nvim/init.lua
@@ -27,23 +27,58 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>gg", "<cmd>Neogit cwd=%:p:h<cr>")
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 vim.keymap.set("n", "<leader>bf", function() vim.lsp.buf.format() end, { desc = "Format buffer using LSP" })
-local group = vim.api.nvim_create_augroup("AutoWrite", { clear = true })
-vim.api.nvim_create_autocmd({ "InsertLeave", "BufWritePre" }, {
-	group = group,
+
+vim.api.nvim_create_autocmd({ "InsertLeave", }, {
+	group = vim.api.nvim_create_augroup("AutoWrite", { clear = true }),
 	callback = function(args)
 		if #vim.lsp.get_clients({ bufnr = args.buf }) > 0 then
-			vim.lsp.buf.format()
 			vim.cmd("write")
 		end
 	end
 })
 
-local group = vim.api.nvim_create_augroup("ConfigReload", { clear = true })
+vim.api.nvim_create_autocmd({ "BufWritePre", "InsertLeave" }, {
+	group = vim.api.nvim_create_augroup("AutoFormat", { clear = true }),
+	callback = function(args)
+		if #vim.lsp.get_clients({ bufnr = args.buf }) > 0 then
+			vim.lsp.buf.format()
+		end
+	end
+})
+
 vim.api.nvim_create_autocmd("BufWritePost", {
 	pattern = { ".nvim.lua", "init.lua" },
-	group = group,
+	group = vim.api.nvim_create_augroup("ConfigReload", { clear = true }),
 	callback = function(args)
 		vim.cmd("silent source " .. vim.fn.fnameescape(args.file))
 		vim.schedule(function() vim.notify("Lua file reloaded") end)
 	end
+})
+
+
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+	group = vim.api.nvim_create_augroup("ProjectNvimLua", { clear = true }),
+	callback = function(args)
+		local file = args.file
+		if file == "" then
+			return
+		end
+
+		local root = vim.fs.root(file, ".git")
+		if not root then
+			return
+		end
+
+		local project_config = vim.fs.joinpath(root, ".nvim.lua")
+
+		if vim.fn.filereadable(project_config) == 1 then
+			-- avoid re-sourcing repeatedly for every buffer
+			if vim.g._last_project_nvim_lua ~= project_config then
+				vim.g._last_project_nvim_lua = project_config
+
+				vim.cmd("source " .. vim.fn.fnameescape(project_config))
+				vim.notify("Loaded " .. project_config)
+			end
+		end
+	end,
 })
